e0965b3a0d844f8bffca4df2345dd0abe04b23a6
ProoofAdmin <admin@prooof.in>
2026-02-25 18:18:33 +0530

    Intial Commit for CSS

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const headingStyleCenter = {textAlign:'center'};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='App'>Hello, Employees! - External CSS</h1>
+      <h2 style={{textAlign:'center'}}>Hello, Employees! - inline Css</h2>
+      <h3 style={headingStyleCenter}>Hello, Employees! - inline Css with variable</h3>
     </div>
   );
 }
