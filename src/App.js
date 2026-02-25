647e897bbc2f4f0f82060115d2b418298b2d45a1
ProoofAdmin <admin@prooof.in>
2026-02-25 19:10:52 +0530

    Replacing with variables

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const heading1 = "Hello, Employees! - External CSS";
+  const heading2 = "Hello, Employees! - inline Css";
+  const heading3 = "Hello, Employees! - inline Css with variable";
   const headingStyleCenter = {textAlign:'center'};
   return (
     <div>
-      <h1 className='App'>Hello, Employees! - External CSS</h1>
-      <h2 style={{textAlign:'center'}}>Hello, Employees! - inline Css</h2>
-      <h3 style={headingStyleCenter}>Hello, Employees! - inline Css with variable</h3>
+      <h1 className='App'>{heading1}</h1>
+      <h2 style={{textAlign:'center'}}>{heading2}</h2>
+      <h3 style={headingStyleCenter}>{heading3}</h3>
     </div>
   );
 }
